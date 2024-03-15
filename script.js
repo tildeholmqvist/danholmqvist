a88b8ac6e66961d5ef5e37fad94a8da321b027d2
Tilde Holmqvist <tildeholmqvist@gmail.com>
2024-03-15 13:15:47 +0000

    added a popup functions to the gallery

--- script.js
+++ script.js
@@ -1,19 +1,46 @@
+function openPopup(imageSrc) {
+    const popupImage = document.querySelector('.popup-image');
+    popupImage.src = imageSrc;
+    const enlargedImagePopup = new bootstrap.Modal(document.getElementById('enlargedImagePopup'));
+    enlargedImagePopup.show();
+}
+
+function closePopup() {
+    document.getElementById('enlargedImagePopup').style.display = 'none';
+    document.body.style.overflow = 'auto'; // Enable scrolling on the body
+    var backdrop = document.getElementsByClassName('modal-backdrop');
+        for (var i = 0; i < backdrop.length; i++) {
+            backdrop[i].parentNode.removeChild(backdrop[i]);
+    }
+}
+
+
 document.addEventListener("DOMContentLoaded", function () {
+
     var firstCarousel = document.querySelector('#carouselExampleFeatured');
     var firstCarouselInstance = new bootstrap.Carousel(firstCarousel, {
         interval: 5000,
-        wrap: true 
+        wrap: true
     });
 
     var secondCarousel = document.querySelector('#carouselExampleControls');
     var secondCarouselInstance = new bootstrap.Carousel(secondCarousel, {
         interval: 5000,
-        wrap: true 
+        wrap: true
     });
 
     var thirdCarousel = document.querySelector('#carouselExampleIndicators');
     var thirdCarouselInstance = new bootstrap.Carousel(thirdCarousel, {
         interval: 5000,
-        wrap: true 
+        wrap: true
     });
+
+const artworkContainers = document.querySelectorAll('.artwork-container');
+
+artworkContainers.forEach(container => {
+    container.addEventListener('click', () => {
+        const artworkImage = container.querySelector('.card-img-top').src;
+        openPopup(artworkImage);
+    });
+});
 });
